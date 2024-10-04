35398229005e47798c06136c5c45c7b59a3e2134
Jose Miguel <agredo.jose@correounivalle.edu.co>
2024-10-04 14:52:20 -0500

    se agrego nombre materia

--- tareaGit.js
+++ tareaGit.js
@@ -1,1 +1,1 @@
-console. log ("Jose Miguel Agredo Estupiñan (2325045-3744)-Samuel Silva Ramirez (2343278-3744)")
+console. log ("Jose Miguel Agredo Estupiñan (2325045-3744)-Samuel Silva Ramirez (2343278-3744) Metodologias del desarrollo de software")
